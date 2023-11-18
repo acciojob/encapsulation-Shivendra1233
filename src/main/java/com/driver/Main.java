3eac9f983eae9a7a63a135197a20b0089108dd3a
Shivendra1233 <132485033+Shivendra1233@users.noreply.github.com>
2023-11-17 19:33:22 -0800

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+   public static void main(String[] args) {
+        // Task 3: Create an object of class RWOnly
+        RWOnly obj = new RWOnly();
+
+        // Task 4: Try setting a value to 'name' directly using obj
+        // and also try printing it (Note: This will result in a compilation error)
+        // obj.name = "John"; // Uncommenting this line will result in a compilation error
+        // System.out.println(obj.name); // Uncommenting this line will result in a compilation error
+
+        // Task 6: Try setting a value to 'name' using setter function and access it using getter function
+        obj.setName("John");
+        System.out.println("Name: " + obj.getName());
+    }
 }
