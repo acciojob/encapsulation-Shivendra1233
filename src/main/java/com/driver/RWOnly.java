554c5d2621a5c7fcf5f312df58ffeeefe442f34c
Shivendra1233 <132485033+Shivendra1233@users.noreply.github.com>
2023-11-17 19:32:23 -0800

    RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+     private String name;
+
+    // Task 5: Implement getter and setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
